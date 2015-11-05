1c584fd5ba7dbe34e32a0195e07a9c253b3358ee
Benjamin Toll <benjam72@yahoo.com>
2015-11-04 20:24:14 -0500

    Created printer/ with html.js and log.js printers
    Moved the logic to create the html doc out of bin/dump_describes so
    there can be one or more different printers to choose from.
    
    The log printer will dump to stdout.

---
+++ printer/html.js
@@ -0,0 +1,99 @@
+(() => {
+    'use strict';
+    let fs = require('fs');
+
+    module.exports = {
+        indent: 0,
+
+        captureRow: function (name) {
+            this.rows.push(
+                `<p style="padding-left: ${this.indent * 50}px;" class="${this.indent < 2 ? 'stripe' : ''}"><span>(describe)</span>${name}</p>\n\t\t`
+            );
+        },
+
+        init: function (results) {
+            console.log('Just a moment while I create your sweet suite...');
+
+            for (let m of results.entries()) {
+                let suiteName = m[0],
+                    newFile = suiteName + '_suite.html',
+                    curried = this.makeTpl.bind(null, suiteName),
+                    tpl;
+
+                this.print(m[1]);
+                tpl = curried(this.rows.join(''));
+
+                fs.writeFile(newFile, tpl, 'utf8', (err) => {
+                    if (err) {
+                        console.log('[ERROR] Oh no, something went wrong!');
+                    } else {
+                        console.log('Suite ' + newFile + ' created successfully!');
+                    }
+                });
+            }
+        },
+
+        makeTpl: (header, suite) => {
+            return `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset="utf-8">
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                    }
+
+                    h3 {
+                        background: #e5a552;
+                        border: 0px solid #000;
+                        border-top-width: 2px;
+                        padding: 10px 10px 10px 20px;
+                    }
+
+                    p {
+                        padding: 10px;
+                    }
+
+                    p.stripe {
+                        background: #ddd;
+                        border: 0px solid #000;
+                        border-top-width: 2px;
+                    }
+
+                    span {
+                        color: #207ab2;
+                        margin-right: 5px;
+                    }
+                </style>
+                </head>
+
+                <body>
+                    <h3>${header}</h3>
+                    ${suite}
+                </body>
+                </html>
+            `;
+        },
+
+        print: function (map) {
+            let me = this;
+
+            me.indent++;
+
+            for (let entry of map.entries()) {
+                me.captureRow(entry[0]);
+
+                if (entry[1].size) {
+                    me.print(entry[1]);
+                }
+            }
+
+            me.indent--;
+        },
+
+        rows: []
+    };
+})();
+
---
+++ printer/log.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+
+(() => {
+    'use strict';
+
+    let getTabs = (indent) => {
+        let tabs = '';
+
+        while (indent) {
+            return getTabs(--indent) + '\t';
+        }
+
+        return tabs;
+    };
+
+    module.exports = {
+        indent: 0,
+
+        init: function (results) {
+            for (let m of results.entries()) {
+                console.log(`Dumping results for suite ${m[0]}:\n`);
+                this.print(m[1]);
+            }
+        },
+
+        captureRow: function (name) {
+            console.log(`${getTabs(this.indent)}(describe) ${name}`);
+        },
+
+        print: function (map) {
+            let me = this;
+
+            me.indent++;
+
+            for (let entry of map.entries()) {
+                me.captureRow(entry[0]);
+
+                if (entry[1].size) {
+                    me.print(entry[1]);
+                }
+            }
+
+            me.indent--;
+        }
+    };
+})();
+
