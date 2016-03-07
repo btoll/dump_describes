1956f5cd8da04d08b55ab188c8197fa15f807b1b
Benjamin Toll <benjam72@yahoo.com>
2016-03-07 11:27:04 -0500

    Added ./scripts/watch.bash watch script

---
+++ scripts/watch.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+if which entr > /dev/null; then
+    shopt -s globstar
+    ls ./src/**/*.js ./spec/*spec.js | entr -c npm run specs
+else
+    echo "$(tput setaf 1)[ERROR]$(tput sgr0) This script has a dependency on entr (http://entrproject.org/) to watch files."
+    exit 1
+fi
+
